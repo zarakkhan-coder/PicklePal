df5d18bbde29058d18921a7806bdbcafb751d1b0
zarakkhan-coder <zarak.khan@walmart.com>
2025-08-29 21:28:30 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '../styles/globals.css';
+// pages/_app.js
+import Head from "next/head";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Makes layout match device width and respects iOS safe areas */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+        <meta name="theme-color" content="#0b1b2a" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
